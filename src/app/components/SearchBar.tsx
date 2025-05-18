16f0dd67d3dfbc02e5a588a17677acedd1e8d484
Cristian Montoya <ckcristock@gmail.com>
2025-05-18 17:08:50 -0500

    fix SearchBar input bg-color

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -194,9 +194,12 @@ const SearchBar: React.FC<SearchBarProps> = ({
         ref={inputRef}
         type="text"
         placeholder="Search for a word…"
-        className={`w-full rounded-xl px-5 py-3 text-base sm:text-lg shadow-sm focus:outline-none focus:ring-2 ${
-          error ? "ring-2 ring-red-500" : "focus:ring-purple-600"
-        }`}
+        className={`
+          w-full rounded-xl px-5 py-3 text-base sm:text-lg shadow-sm focus:outline-none focus:ring-2
+          transition-colors duration-300
+          ${error ? "ring-2 ring-red-500" : "focus:ring-purple-600"}
+          ${theme === "light" ? "bg-[#f4f4f4]" : "bg-zinc-900"}
+        `}
         value={search}
         onChange={(e) => {
           setSearch(e.target.value);
